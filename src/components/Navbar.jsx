ff0fa4045c09cfdb0d4a03b895abfc0a5963385f
johnmulder8 <dvdmr316@gmail.com>
2026-06-03 01:34:12 -0400

    feat: copy-pate-in-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,6 +52,25 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
